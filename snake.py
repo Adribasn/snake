85087697cd982355cd7f07f12d2e3be7da733927
Adrien Basyn <adrien.basyn@outlook.com>
2023-02-11 17:50:32 +0100

    Fully functional snake game

--- snake.py
+++ snake.py
@@ -7,11 +7,10 @@ class Snake():
         self.biasX = 0
         self.biasY = 0
         self.length = 1
-        self.score = 0
         self.alive = True
         self.direction = 'right'
-        self.food = [random.randint(1, 46), random.randint(9, 54)]
-        pyxel.init(47, 55, title="Snake", fps=15)
+        self.food = [random.randint(1, 45), random.randint(10, 53)]
+        pyxel.init(47, 55, title="Snake", fps=18)
         pyxel.run(self.update, self.draw)
 
     def update(self):       
@@ -20,6 +19,8 @@ class Snake():
             self.eat()
             self.updateSnake()
         else:
+            pyxel.sound(1).set("a3a2c2c2", "n", "7742", "s", 10)
+            pyxel.play(1, 1)
             self.reset()
     
     def updateDirection(self):
@@ -64,8 +65,14 @@ class Snake():
         oldHead = self.body[0]
         newHead = [oldHead[0] + self.biasX, oldHead[1] + self.biasY]
 
-        if not (1 < newHead[0] < 45 and 9 < newHead[1] < 53):
+        if not (0 < newHead[0] < 46 and 8 < newHead[1] < 54):
             self.alive = False
+            return
+        
+        for segment in self.body:
+            if newHead == segment:
+                self.alive = False
+                return
 
         self.body.insert(0, newHead)
         self.body = self.body[:self.length]
@@ -76,8 +83,11 @@ class Snake():
     def eat(self):
         if self.body[0][0] == self.food[0] and self.body[0][1] == self.food[1]:
             self.length += 1
-            self.food[0] = random.randint(0, 44)
-            self.food[1] = random.randint(8, 52)
+            self.food[0] = random.randint(1, 45)
+            self.food[1] = random.randint(10, 53)
+
+            pyxel.sound(0).set("a3a2c2c2", "p", "7742", "s", 10)
+            pyxel.play(0, 0)
     
     def reset(self):
         self.body = [[1, 20]] 
@@ -87,17 +97,18 @@ class Snake():
         self.score = 0
         self.alive = True
         self.direction = 'right'
-        self.food = [random.randint(0, 44), random.randint(8, 52)]
+        self.food = [random.randint(1, 45), random.randint(10, 53)]
 
     def draw(self):
         pyxel.cls(14)
         pyxel.rectb(0, 0, 47, 55, 0)
         pyxel.rect(1, 8, 47, 1, 0)
+        pyxel.text(2, 2, "SCORE : " + str(self.length - 1), 0)
 
         #Display each item of the snake body
         for segment in self.body:
-            pyxel.rect(segment[0], segment[1], 1, 1, 8)
+            pyxel.rect(segment[0], segment[1], 1, 1, 0)
         
-        pyxel.rect(self.food[0], self.food[1], 1, 1 , 2)
+        pyxel.rect(self.food[0], self.food[1], 1, 1 , 8)
 
 Snake()
